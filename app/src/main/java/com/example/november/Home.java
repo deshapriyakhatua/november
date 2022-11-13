f922981d990254e2dbb861df0a64e86ffef8b101
deshapriya <deshapriyakhatua@gmail.com>
2022-11-13 20:39:45 +0530

    status bar hiding done

--- app/src/main/java/com/example/november/Home.java
+++ app/src/main/java/com/example/november/Home.java
@@ -1,25 +1,21 @@
 package com.example.november;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // fullScreen activity
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(1);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-
         setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
+
+
     }
 }
