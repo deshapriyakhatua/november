2baa49754977fa5f5360319083cc283eb0d18a6c
deshapriya <deshapriyakhatua@gmail.com>
2022-11-14 17:17:49 +0530

    disable back press in home page

--- app/src/main/java/com/example/november/Home.java
+++ app/src/main/java/com/example/november/Home.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -12,10 +13,18 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // hiding action bar
         getSupportActionBar().hide();
 
+        // hiding status bar
         WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
 
 
+    }
+
+    // disable backPress Button
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(Home.this,"Already in Home Screen",Toast.LENGTH_SHORT).show();
     }
 }
