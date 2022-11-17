d9fa0ee8442af537012a680bec6fe0cacd74b802
deshapriya <deshapriyakhatua@gmail.com>
2022-11-17 22:42:49 +0530

    17 11 2022

--- app/src/main/java/com/example/november/Questions.java
+++ app/src/main/java/com/example/november/Questions.java
@@ -1,10 +1,19 @@
 package com.example.november;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Questions extends AppCompatActivity {
 
@@ -19,6 +28,28 @@ public class Questions extends AppCompatActivity {
         // hiding status bar
         WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
 
+        // firebse
+        TextView question = findViewById(R.id.textView);
+        RadioButton qs1 = findViewById(R.id.radioButton);
+        RadioButton qs2 = findViewById(R.id.radioButton2);
+        RadioButton qs3 = findViewById(R.id.radioButton3);
+        RadioButton qs4 = findViewById(R.id.radioButton4);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://november-a9a10-default-rtdb.firebaseio.com/");
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.child("Java").getChildren()){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
     // disable backPress Button
